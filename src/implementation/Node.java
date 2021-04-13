e43728985b4040218b39193060c52e8c4a485ae1
hrincredible007 <hritikbhardwaj2002@gmail.com>
2021-04-13 22:06:49 +0530

    Node class

--- src/implementation/Node.java
+++ src/implementation/Node.java
@@ -1,5 +1,37 @@
 package implementation;
 
 public class Node<E extends Comparable<E>> implements myinterface.Node<E> {
-    //complete node class
+    private E data;
+    Node<E> left;
+    Node<E> right;
+
+    public Node(E data) {
+        this.data = data;
+        left = right  = null;
+    }
+
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public Node<E> getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node<E> left) {
+        this.left = left;
+    }
+
+    public Node<E> getRight() {
+        return right;
+    }
+
+    public void setRight(Node<E> right) {
+        this.right = right;
+    }
 }
+
