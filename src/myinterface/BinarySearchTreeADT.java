7e625d933f67bf7bb7aca6c3305aeef797cc46e0
hrincredible007 <hritikbhardwaj2002@gmail.com>
2021-04-13 22:18:49 +0530

    Binary Search Tree ADT

--- src/myinterface/BinarySearchTreeADT.java
+++ src/myinterface/BinarySearchTreeADT.java
@@ -3,9 +3,9 @@ package myinterface;
 public interface BinarySearchTreeADT<E extends Comparable<E>> {
     void insert(E data);
     boolean search(E searchElement);
-    void inOrder(Node<E> node);
-    void preOrder(Node<E> node);
-    void postOrder(Node<E> node);
+    void inOrder(implementation.Node<E> node);
+    void preOrder(implementation.Node<E> node);
+    void postOrder(implementation.Node<E> node);
     void reverseInOrder(Node<E> node);
     void delete(E data);
     int height(Node<E> node);
