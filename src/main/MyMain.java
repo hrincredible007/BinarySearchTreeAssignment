fc33bde72e5d76f38b2556236d8b24dd4b041072
hrincredible007 <hritikbhardwaj2002@gmail.com>
2021-04-13 22:47:13 +0530

    Main Method

--- src/main/MyMain.java
+++ src/main/MyMain.java
@@ -1,8 +1,25 @@
 package main;
 
+import implementation.MyBinarySearchTree;
+import myinterface.BinarySearchTreeADT;
+
 public class MyMain {
     public static void main(String[] args) {
-        //create binary search tree
-        // and perform insert, search, traversal, delete
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        tree.insert(66);
+        tree.insert(45);
+        tree.insert(50);
+        tree.insert(12);
+        tree.insert(70);
+        tree.insert(25);
+        tree.insert(45);
+        tree.inOrder(tree.getRoot());
+        tree.postOrder(tree.getRoot());
+        tree.preOrder(tree.getRoot());
+        tree.search(25);
+
+
+
+
     }
 }
